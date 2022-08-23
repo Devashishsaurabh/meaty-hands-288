b0e3c81ab6edb43eab21fac74e4839e216bc1278
snoorbasha50 <97452851+snoorbasha50@users.noreply.github.com>
2022-08-23 22:16:17 +0530

    updated

--- src/Pages/KindMoments.jsx
+++ src/Pages/KindMoments.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from "./Kindmoments.module.css"
 
 const KindMoments = () => {
   return (
-    <div>KindMoments</div>
+    <div>
+     <div className={styles.top}>
+
+      <div className={styles.topleft}>
+        <div className={styles.box1}>
+          <div className={styles.toplefttop}>
+           <div className={styles.headingptag}>Kind Moments</div>
+           <div className={styles.headingptag}>| Saving Lives with Yummy Photos</div>
+          </div>
+          <div className={styles.about}> 
+               Discover new, delicious meat-free cuisines enjoyed by other
+                compassionate food lovers. Share your interesting KindMoments,
+                and build your own fans and followers to encourage kind, healthy
+                meals.
+          </div>
+          <div className={styles.about}>Download our mobile app to easily showcase your masterpieces and
+                interact with food lovers.</div>
+          <div className={styles.about}>Get started sharing your tasty moments now.</div>
+        </div>
+      </div>
+
+
+
+      <div className={styles.topright}></div>
+
+     </div>
+    </div>
   )
 }
 
