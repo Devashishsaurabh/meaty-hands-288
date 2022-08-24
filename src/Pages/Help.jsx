65b81153527898fd557e3fc9827f430600a8a8b1
rovin76 <rovin7678>
2022-08-24 10:49:55 +0530

    rovin singh

--- src/Pages/Help.jsx
+++ src/Pages/Help.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import styled from "./Pages.module.css";
 const Help = () => {
+  let sideContent = [
+    {
+      title: "General Help",
+    },
+    {
+      title: "Merchant Help",
+    },
+    {
+      title: "Advertising",
+    },
+    {
+      title: "About KindMeal",
+    },
+    {
+      title: "Superhero Guide",
+    },
+    {
+      title: "Contact Us",
+    },
+  ];
   return (
-    <div>Help</div>
-  )
-}
+    <div className={styled.helpmain}>
+      <div>
+        {sideContent.map((item) => (
+          <div key={item.title} className={styled.helpcard}>
+            <img src="https://www.kindmeal.my/images/join_normal.png" alt="img" width="100px"></img>
+            <h4>{item.title}</h4>
+          </div>
+        ))}
+      </div>
+      <div>Content</div>
+    </div>
+  );
+};
 
-export default Help
+export default Help;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Footer from '../Components/Footer'
-import Navbar from '../Components/Navbar'
+import React from "react";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
-   
     <div>
-       <Navbar/>
-            <h1>Home Page</h1>
-          <Footer/>
+      <Navbar />
+      <h1>Home Page</h1>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
